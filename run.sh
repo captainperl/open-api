7de47df0eece2b367813a7774c174c5aae16d39a
michal <michal.folcan@univie.ac.at>
2021-04-06 11:24:40 +0200

    delete "$API_KEY"

--- run.sh
+++ run.sh
@@ -2,11 +2,11 @@
 
 set -e
 
-if [ "$API_KEY" != "**None**" ]; then
-  sed -i "s|/\*||g" index.html
-  sed -i "s|\*/||g" index.html
-  sed -i "s|myApiKeyXXXX123456789|$API_KEY|g" index.html
-fi
+# if [ "$API_KEY" != "**None**" ]; then
+#   sed -i "s|/\*||g" index.html
+#   sed -i "s|\*/||g" index.html
+#   sed -i "s|myApiKeyXXXX123456789|$API_KEY|g" index.html
+# fi
 
 if [[ -f $SWAGGER_JSON ]]; then
   sed -i "s|http://petstore.swagger.io/v2/swagger.json|/swagger.json|g" index.html
